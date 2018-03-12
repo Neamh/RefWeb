6c1049cc4cfaa91959d4946cb5fe9f06385caf91
Neamh <rene@schweinberger.name>
2018-03-12 10:19:46 +0100

    Erstes Einchecken des Moduls RefWeb

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace RefWeb.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public string LicenseNumber {get; set;}
+        public string LicenseLevel { get; set; }
+
+
+    }
+}
---
+++ Models/ManageViewModels/RefProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RefWeb.Models.ManageViewModels
+{
+    public class RefProfileViewModel
+    {
+        public string UserName { get; set; }
+        public string LicenseNumber { get; set; }
+        public string LicenseLevel { get; set; }
+    }
+}
